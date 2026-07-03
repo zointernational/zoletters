95811d7896c2f63d4856814df0ec655b1b93edc9
zointernational <admin@zointernational.in>
2026-07-03 09:37:42 +0000

    fix: downgrade Laravel to v10 to avoid security advisories

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,20 +8,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'check.installation' => \App\Http\Middleware\CheckInstallation::class,
-        ]);
-        
-        $middleware->web(append: [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
